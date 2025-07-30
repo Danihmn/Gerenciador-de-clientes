40d56e95715e22c488c72b388dd34f6905e6e670
Danihmn <daniel.bezerra.mult@gmail.com>
2025-07-29 23:39:39 -0300

    Adiciona classe para testar as inserções feitas no banco de dados

--- ConexaoBancoDeDados.cs
+++ ConexaoBancoDeDados.cs
@@ -8,13 +8,33 @@ namespace AppBancoDeDados
     /// </summary>
     internal class Conexao
     {
-        #region Obtem a conexao com o banco
+        // Conexão para testes
+        private static SQLiteConnection _conexaoTeste;
+
+        #region Obtem a conexao com o banco temporario
         /// <summary>
-        /// Método responsável por estabelecer conexão com o banco
+        /// Método responsável por atribuir à variável de conexão uma conexão temporária
+        /// em memória, para os testes não interferirem no banco de dados físico do projeto
+        /// </summary>
+        /// <param name="conexao">Conexão temporária a ser passada</param>
+        public static void ObterConexaoParaTestes(SQLiteConnection conexao)
+        {
+            _conexaoTeste = conexao; // A variável de conexão recebe a conexão temporária de memória para teste
+        }
+        #endregion
+
+        #region Obtem a conexao com o banco fisico
+        /// <summary>
+        /// Método responsável por estabelecer conexão com o banco físico
         /// </summary>
         /// <returns>Retorna a conexão aberta</returns>
         public static SQLiteConnection ObterConexao()
         {
+            // Verifica se a conexão de testes está sendo usada, ou seja, se estão havendo testes
+            if (_conexaoTeste != null)
+                return _conexaoTeste; // Retorna a conexão de testes
+
+            // Caso não estiverem havendo testes, estabelece conexão normal com o banco de dados físico
             SQLiteConnection conexao = new SQLiteConnection(@"Data Source = C:\\Users\\danie\\Dev.NET\\Estudos\\AppBancoDeDados\\database.db");
             conexao.Open(); // Abre a conexão
 
---
+++ InsercoesTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Data.SQLite;
+
+namespace AppBancoDeDados
+{
+    public class InsercoesTests
+    {
+        private SQLiteConnection _conexao; // Será a conexão temporária
+
+        /// <summary>
+        /// Método responsável por estabelecer a conexão com o banco temporário.
+        /// Cria o comando de criar uma nova tabela de clientes
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            // Passa como conexão um banco em memória
+            _conexao = new SQLiteConnection("Data Source=:memory:;Version=3;New=True;");
+            _conexao.Open();
+
+            // Cria nova tabela
+            var comando = _conexao.CreateCommand();
+            comando.CommandText = @"
+                CREATE TABLE clientes(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT,
+                    email TEXT,
+                    telefone TEXT
+                );
+            ";
+            comando.ExecuteNonQuery();
+
+            // Substitui a conexão com o banco físico com uma conexão com o banco temporário
+            Conexao.ObterConexaoParaTestes(_conexao);
+        }
+
+        /// <summary>
+        /// Método responsável por testar as inserções feitas no banco
+        /// </summary>
+        [TestMethod]
+        public void InserirDados_DeveInserirDadosNoBanco()
+        {
+            // Act
+            Insercao.InserirDados("Daniel", "daniel.bezerra.mult@outlook.com", "19993054611");
+
+            // Assert
+            var comando = _conexao.CreateCommand();
+            comando.CommandText = "SELECT COUNT(*) FROM clientes WHERE nome = @nome;"; // Query que conta as linhas na consulta
+            comando.Parameters.AddWithValue("@nome", "Daniel");
+
+            // Converte em um long a quantidade de linhas
+            long count = (long)comando.ExecuteScalar();
+
+            // Se for o mesmo valor do long, então passou no teste
+            Assert.AreEqual(1, count);
+        }
+
+        /// <summary>
+        /// Método responsável por fechar a conexão temporária, descartando a inserção de teste
+        /// </summary>
+        [TestCleanup]
+        public void LimparTeste()
+        {
+            _conexao.Close(); // Fecha a conexão
+        }
+    }
+}
